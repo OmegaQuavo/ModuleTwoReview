87f1d145dfca1eee73fd41082e2409576a00c9ba
Caleb <cedinsmore63@gmail.com>
2025-10-04 21:38:26 -0400

    added student class, addgrade method and calculateaveragegrade

--- ModuleTwoReview/Program.cs
+++ ModuleTwoReview/Program.cs
@@ -1,3 +1,40 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using System;
+using System.Collections.Generic;
 
-Console.WriteLine("Hello, World!");
+namespace StudentGradesApp
+{
+    public class Student
+    {
+        public string Name { get; set; }
+        public int ID { get; set; }
+        public List<double> Grades { get; set; }
+
+        public Student()
+        {
+            Grades = new List<double>();
+        }
+
+        public void AddGrade(double grade)
+        {
+            Grades.Add(grade);
+        }
+
+        public void AddGrade(params double[] grades)
+        {
+            Grades.AddRange(grades);
+        }
+
+        public double CalculateAverageGrade()
+        {
+            if (Grades.Count == 0)
+                return 0;
+
+            double total = 0;
+            foreach (double grade in Grades)
+            {
+                total += grade;
+            }
+
+            return total / Grades.Count;
+        }
+    }
